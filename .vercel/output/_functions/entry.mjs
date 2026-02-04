2afdca2fbbfa93b4f70fb681526d199d86c1d62b
890su <iharshastsiuk@gmail.com>
2026-02-04 01:25:43 +0100

    Simplify API

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DHMJD4p5.mjs';
-import { manifest } from './manifest_DkosRa8Z.mjs';
+import { manifest } from './manifest_CRYWB4I3.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -25,7 +25,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "f86028e8-4793-426f-b220-2a7bfdbeed2e",
+    "middlewareSecret": "dc3d8ef6-203f-4652-b888-df199d20f7a1",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
